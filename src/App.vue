159155722a2f9931a8989bea6aed5dc559e3d093
PabloReynaga <pablo.reynaga@bonprix.net>
2024-06-19 23:03:52 +0200

    ADD scss and first config

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
+import Navbar from './components/Navbar.vue';
 
 </script>
 
 <template>
+    <Navbar></Navbar>
  <h1>Project init.</h1>
 </template>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,41 @@
+<script setup></script>
+
+<template>
+  <div class="nav-bar">
+    <div class="title-container">
+      <h1>Notes Manager</h1>
+    </div>
+    <div class="switch-container">
+      <div class="switch-button-container">
+        <button class="switch-button"></button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.nav-bar {
+  display: flex;
+  justify-content: space-between;
+  height: 4rem;
+  background-color: $primary;
+}
+.switch-container {
+  height: 100%;
+}
+.switch-button-container {
+  background-color: $secondary;
+  width: 50px;
+  height: 25px;
+  border: 30px;
+  border-radius: 30px;
+}
+.switch-button {
+  height: 25px;
+  width: 25px;
+  border-radius: 30px;
+  background-color: $tan;
+  border: none;
+  cursor: pointer;
+}
+</style>
