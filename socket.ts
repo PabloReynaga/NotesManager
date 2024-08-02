fd4aea69ec8e7ade774e9ba8627b89d304543052
PabloReynaga <reynagafarias@gmail.com>
2024-08-02 14:51:48 +0200

    ADD - add new packages, socket and starting LogIn page and routing

---
+++ socket.ts
@@ -0,0 +1,10 @@
+import { io } from 'socket.io-client';
+
+const socketUrl = import.meta.env.VITE_SOCKET_URL;
+console.log(socketUrl)
+if (!socketUrl) throw new Error('Missing VITE_SOCKET_URL env.')
+const socket = io(socketUrl, {
+  autoConnect: false
+});
+
+export default socket;
