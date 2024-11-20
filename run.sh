93d10b5e0ec55e1601496111fe005ac9add73214
PabloReynaga <92642558+PabloReynaga@users.noreply.github.com>
2024-11-20 01:06:57 +0100

    CLN - Clean

--- run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-gnome-terminal --tab -- bash -c "cd /home/pablo/Pablo-PC/programacion/NotesManager/frontend  ; npm run dev; exec bash"
-gnome-terminal --tab -- bash -c "cd /home/pablo/Pablo-PC/programacion/NotesManager/backend ; npm run dev; exec bash"
-gnome-terminal --tab -- bash -c "cd /home/pablo/Pablo-PC/programacion/NotesManager/backend ; docker-compose up; exec bash"
